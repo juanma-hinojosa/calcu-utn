5136cd22a9e9a4904ab3f1b7f53370a80ccb36cc
Juan  Hinojosa <juanma.hinojosa97@gmail.com>
2022-06-13 18:00:56 -0300

    segundo commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const suma = document.getElementById("sum")
+const resta = document.getElementById("res")
+const division = document.getElementById("div")
+const multi = document.getElementById("mul")
+
+suma = document.addEventListener(
+    "click", () => {
+        alert("hola")
+    }
+)
