1ab5b1c7f595c6f524143694838aedee57cd18ee
Juan  Hinojosa <juanma.hinojosa97@gmail.com>
2022-06-14 21:22:47 -0300

    alerts resultados

--- js/index.js
+++ js/index.js
@@ -1,10 +1,54 @@
-const suma = document.getElementById("sum")
-const resta = document.getElementById("res")
-const division = document.getElementById("div")
-const multi = document.getElementById("mul")
+// const suma = document.getElementById("sum")
+// const resta = document.getElementById("res")
+// const division = document.getElementById("div")
+// const multi = document.getElementById("mul")
 
-suma = document.addEventListener(
-    "click", () => {
-        
-    }
-)
+
+// const num1 = document.getElementById("num1").value;
+// const num2 = document.getElementById("num2").value;
+
+// suma = document.addEventListener(
+//     "click", () => {
+
+//     }
+// )
+
+function suma() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    if (num1 == "" || num2 == "") {
+        alert("Debes ingresar dos numeros")
+    } else {
+        alert(`Resultado de la operacion entre ${num1} y ${num2} es  ${parseInt(num1) + parseInt(num2)}` );
+    } 
+}
+
+function resta() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    if (num1 == "" || num2 == "") {
+        alert("Debes ingresar dos numeros")
+    } else {
+        alert(`Resultado de la operacion entre ${num1} y ${num2} es  ${parseInt(num1) - parseInt(num2)}` );
+    } 
+}
+
+function division() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    if (num1 == "" || num2 == "") {
+        alert("Debes ingresar dos numeros")
+    } else {
+        alert(`Resultado de la operacion entre ${num1} y ${num2} es  ${parseInt(num1) / parseInt(num2)}` );
+    } 
+}
+
+function multiplicacion() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    if (num1 == "" || num2 == "") {
+        alert("Debes ingresar dos numeros")
+    } else {
+        alert(`Resultado de la operacion entre ${num1} y ${num2} es  ${parseInt(num1) * parseInt(num2)}` );
+    } 
+}
